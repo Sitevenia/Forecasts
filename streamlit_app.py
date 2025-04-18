3b0f981aa520014a6ba6161919570e32cf35e7ae
Sitevenia <oclaus57@gmail.com>
2025-04-19 01:23:52 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -172,8 +172,10 @@ if uploaded_file:
         with pd.ExcelWriter(output, engine="xlsxwriter") as writer:
             df[colonnes_sim1].to_excel(writer, sheet_name="Simulation_1", index=False)
             if colonnes_sim2:
-                if df_sim2 is not None and colonnes_sim2:
-            df_sim2[colonnes_sim2].to_excel(writer, sheet_name="Simulation_2", index=False)
+                try:
+    df_sim2[colonnes_sim2].to_excel(writer, sheet_name="Simulation_2", index=False)
+except:
+    pass
             if comparatif is not None:
                 comparatif.to_excel(writer, sheet_name="Comparatif", index=False)
         
