84b71b783835f2138b4829299728badee7e06c5e
Sitevenia <oclaus57@gmail.com>
2025-04-19 15:43:43 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ def repartir_et_ajuster(total_qte, ventes_n1_mois, conditionnement):
 
             def safe_int(x):
             try:
-            if pd.isna(x) or not np.isfinite(x):
+                if pd.isna(x) or not np.isfinite(x):
             return 0
                 pass
             return int(round(x))
